1637adaf7bea211db64fe314d1aa5f836fbfbb95
Anshul patle <patleanshul920@gmail.com>
2023-03-27 22:52:54 +0530

    Update main.py

--- Expense-app/main.py
+++ Expense-app/main.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def login():
     return render_template('login.html')
 
 @app.route('/register')
 def about():
-    return 'About page'
+    return render_template('register.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
